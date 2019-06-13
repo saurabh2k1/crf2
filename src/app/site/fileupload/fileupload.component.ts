d14fa4ca57f803689502a1cdbe6bdef061ce55da
Saurabh Sharma <saurabh2k1@gmail.com>
2019-06-13 22:56:32 +0530

    bgImagechange

--- src/app/site/fileupload/fileupload.component.ts
+++ src/app/site/fileupload/fileupload.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { SiteService } from '../site.service';
 import { ActivatedRoute } from '@angular/router';
+import { AlertService } from 'src/app/alert.service';
 
 @Component({
   selector: 'app-fileupload',
@@ -17,10 +18,16 @@ export class FileuploadComponent implements OnInit {
   visitID = '';
   pageTitle = 'File Upload';
   patient: any;
+  fileName = '';
+  approvedType: string[] = ['image/jpeg', 'image/png', 'image/tiff'];
+  fileTypeMark = 'close';
+  fileSizeMark = 'close';
+
 
     constructor(
     private siteService: SiteService,
     private fb: FormBuilder,
+    private alert: AlertService,
     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
@@ -30,7 +37,7 @@ export class FileuploadComponent implements OnInit {
       this.patient = data;
     });
     this.form = this.fb.group({
-      file: ['']
+      file: ['', Validators.required]
     });
   }
 
@@ -39,21 +46,48 @@ export class FileuploadComponent implements OnInit {
   }
 
   onFileChange(event) {
+    this.fileTypeMark = 'close';
+    this.fileSizeMark = 'close';
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
-      this.form.get('file').setValue(file);
+      console.log('FIle Type:', file.type);
+      if (this.approvedType.includes(file.type)) {
+        this.fileTypeMark = 'check';
+      } else {
+        this.alert.error('Please upload only images');
+        this.fileTypeMark = 'close';
+      }
+      if (Math.round(file.size / (1024 * 1024)) < 5) {
+        this.fileSizeMark = 'check';
+      } else {
+        this.alert.error('File size must be less than 5 MB');
+        this.fileSizeMark = 'close';
+      }
+      if (this.fileSizeMark === 'check' && this.fileTypeMark === 'check') {
+        this.form.get('file').setValue(file);
+      } else {
+        this.form.get('file').setValue('');
+      }
     }
   }
 
   onSubmit() {
-    const formData = new FormData();
-    formData.append('file', this.form.get('file').value);
-    formData.append('patient_id', this.patID);
-    formData.append('visit_id', this.visitID);
-    this.siteService.uploadFile(formData).subscribe(
-      (res) => this.uploadResponse = res,
-      (err) => this.error = err
-    );
+    if (this.form.valid) {
+      const formData = new FormData();
+      formData.append('file', this.form.get('file').value);
+      formData.append('patient_id', this.patID);
+      formData.append('visit_id', this.visitID);
+      this.siteService.uploadFile(formData).subscribe(
+        (res) => {
+          this.uploadResponse = res;
+          if (res.status === 'success') {
+            this.alert.success(res.message);
+            this.fileName = res.filePath;
+          }
+        },
+        (err) => { this.error = err; this.alert.error(err); }
+      );
+    }
   }
 
 
